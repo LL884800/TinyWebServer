36d6d366bbbb4337e535c9a4888342925c247b28
LL884800 <152848653+LL884800@users.noreply.github.com>
2025-04-07 17:32:14 +0800

    config.cpp

--- config.cpp
+++ config.cpp
@@ -35,45 +35,55 @@ Config::Config(){
 void Config::parse_arg(int argc, char*argv[]){
     int opt;
     const char *str = "p:l:m:o:s:t:c:a:";
+    // 循环解析命令行参数，直到没有更多的选项
+    // 标准库函数，用于解析命令行选项
     while ((opt = getopt(argc, argv, str)) != -1)
     {
         switch (opt)
         {
+            // 端口号
         case 'p':
         {
             PORT = atoi(optarg);
             break;
         }
+            // 日志写入方式
         case 'l':
         {
             LOGWrite = atoi(optarg);
             break;
         }
+            // 触发组合模式
         case 'm':
         {
             TRIGMode = atoi(optarg);
             break;
         }
+            // 优雅关闭连接选项
         case 'o':
         {
             OPT_LINGER = atoi(optarg);
             break;
         }
+            // 数据库连接池数量
         case 's':
         {
             sql_num = atoi(optarg);
             break;
         }
+            // 线程池内的线程数量
         case 't':
         {
             thread_num = atoi(optarg);
             break;
         }
+            // 是否关闭日志
         case 'c':
         {
             close_log = atoi(optarg);
             break;
         }
+            // 并发模型
         case 'a':
         {
             actor_model = atoi(optarg);
